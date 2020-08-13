fab2eda54dbe453ac832802b3da87d0d927b4a78
Ivan Jeftic <ivan.jeftic@git.seavus.com>
2020-08-13 22:59:45 +0200

    change proxy to localhost

--- src/main/resources/shop/src/app/app-constants.ts
+++ src/main/resources/shop/src/app/app-constants.ts
@@ -1,6 +1,6 @@
 export class AppConstants {
 
-    public static readonly baseUrl: string = "https://novko.herokuapp.com/";
+    public static readonly baseUrl: string = "http://localhost:8080/";
     public static readonly isLoggedInUser: string = "isLoggedInUser";
     public static readonly isLoggedInAdmin: string = "isLoggedInAdmin";
     public static readonly role: string = "role";
