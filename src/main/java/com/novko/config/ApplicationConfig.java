b870061340070b19ed87607097a8c058fc72b367
Ivan Jeftic <Ivan.Jeftic@seavus.com>
2021-01-29 22:38:44 +0100

    config database

--- src/main/java/com/novko/config/ApplicationConfig.java
+++ src/main/java/com/novko/config/ApplicationConfig.java
@@ -36,9 +36,9 @@ public class ApplicationConfig {
         DriverManagerDataSource ds = new DriverManagerDataSource();
         ds.setDriverClassName("org.postgresql.Driver");
 
-		ds.setUrl("jdbc:postgresql://ec2-54-78-127-245.eu-west-1.compute.amazonaws.com:5432/dc9ihbk205tp76");
-		ds.setUsername("oibquhcibuhqzs");
-		ds.setPassword("d3bdcdcc82f3b88d68198b1551c9dd8006446090a1b77dad13ff406fc1da2f49");
+        ds.setUrl("jdbc:postgresql://ec2-54-78-127-245.eu-west-1.compute.amazonaws.com:5432/dc9ihbk205tp76");
+        ds.setUsername("oibquhcibuhqzs");
+        ds.setPassword("d3bdcdcc82f3b88d68198b1551c9dd8006446090a1b77dad13ff406fc1da2f49");
 //        ds.setUrl("jdbc:postgresql://db-postgresql-nyc1-84649-do-user-7801116-0.b.db.ondigitalocean.com:25060/defaultdb");
 //		ds.setUsername("doadmin");
 //		ds.setPassword("xitr8qvmofja425m");
