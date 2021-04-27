d73b692761e916e9c9d015f75cc490cd583dd2e3
Ivan Jeftic <Ivan.Jeftic@seavus.com>
2021-04-27 15:14:58 +0200

    B: WAR file

--- src/main/java/com/novko/NovkoApplication.java
+++ src/main/java/com/novko/NovkoApplication.java
@@ -2,13 +2,14 @@ package com.novko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
 import com.novko.config.MainConfig;
 
 @SpringBootApplication
 @Import({ MainConfig.class })
-public class NovkoApplication {
+public class NovkoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NovkoApplication.class, args);
